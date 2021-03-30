d6b42088f3830cde71bdfb92eec714291702637f
Fergus Cooper <fcooper8472@gmail.com>
2021-03-30 21:54:44 +0100

    Add actual functionality test

--- tests/testthat/test-fbind.R
+++ tests/testthat/test-fbind.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fbind() binds factor (or character)", {
+  x <- c("a", "b")
+  x_fact <- factor(x)
+  y <- c("c", "d")
+  z <- factor(c("a", "b", "c", "d"))
+
+  expect_identical(fbind(x, y), z)
+  expect_identical(fbind(x_fact, y), z)
 })
