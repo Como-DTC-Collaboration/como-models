fc262875391cca5946722d37abc1c34e348e8826
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-04-08 10:29:32 +0100

    Add the age-structured model

---
+++ R/age_model_class.R
@@ -0,0 +1,197 @@
+library(methods)
+library(deSolve)
+library(glue)
+library(ggplot2)
+
+setClass('age_model',
+         #' Defines an age-structured simple SEIR model.
+         #' Slots of this class are as follows:
+         #' 1. `name` character representing name of model
+         #' 2. `output_names` names of the compartments which are used by the
+         #'     model.
+         #' 3. `parameter_names` names of the initial conditions and parameters
+         #'     which are used by the model.
+         #' 4. `parameters` named list containing parameters of the model.
+         #'     Initial values for each compartment, S0, E0, I0, R0.
+         #'     Other parameters: a, b, c which represent the rates of changes
+         #'     between the compartments.
+         #' 5. `n_age_categories` number of age categories.
+
+         #slots
+         slots = c(
+           name = 'character',
+           output_names = 'list',
+           parameter_names = 'list',
+           parameters = 'list',
+           n_age_categories = 'numeric'
+         ),
+
+         #prototypes for the slots, automatically set output and param
+         #names
+         prototype = list(
+           name = NA_character_,
+           output_names = list('S', 'E', 'I', 'R', 'Incidence'),
+           parameter_names = list('S0', 'E0', 'I0', 'R0', 'a', 'b', 'c'),
+           parameters = vector(mode = "list", length = 7),
+           n_age_categories = NA_real_
+
+         )
+)
+
+setGeneric('get_parameters', function(object) standardGeneric('get_parameters'))
+
+setMethod(
+  'get_parameters', 'age_model',
+  function(object){
+    #' Retrieves parameters for an age-structured simple SEIR model.
+
+    return(object@parameters)})
+
+setGeneric(
+  'set_parameters',
+  function(object, S0, E0, I0, R0, a = 1, b = 1, c = 1){
+    standardGeneric('set_parameters')
+  })
+
+setMethod(
+  'set_parameters', 'age_model',
+  function(object, S0, E0, I0, R0, a, b, c) {
+    #' Sets parameters for an age-structured simple SEIR model.
+    #' 
+    #' Default parameters a = 1, b = 1 and c = 1.
+    #' If the initial conditions provided to do not sum to 1 or of different
+    #' sizes compared to the number of age groups, an error is thrown.
+    #' 
+    #' @param params parameters of the model: a, b, and c.
+    #' @param ICs initial conditions of the model, must add to 1.
+    #' 
+    #' @return updated version of the age-structured SEIR model.
+    
+    # check that ICs are valid
+    if (sum(S0, E0, I0, R0) != 1) {
+      stop('Invalid initial conditions. Must add up to 1.')
+    }
+
+    #create list of parameter values
+    params <- list(S0, E0, I0, R0, a, b, c)
+
+    #add names to each value
+    names(params) = object@parameter_names
+
+    #raise errors if age category dimensions do not match initial state vectors
+    #also raise errors if initial state and parameter values are not doubles
+    for (p in list('S0', 'E0', 'I0', 'R0')){
+      if(length(params[[p]]) != object@n_age_categories){
+        stop(glue('Wrong number of age groups for {p}
+              compartments.'))}
+      if(!is.double(params[[p]])){
+        stop(glue('{p} storage format must be a vector.'))}
+    }
+
+    #check format of parameters a and b
+    if(any(length(a) != 1 | length(b) != 1 | length(c) != 1)){
+      stop('The rates of change between compartments are 1-dimensional.')
+    }
+
+    #if all above tests are passed, assign the params namelist to the object
+    object@parameters <- params
+
+    return(object)
+  })
+
+setGeneric(name = 'simulate',
+           def = function(object, times = seq(0, 100, by = 1), is_plot=TRUE){
+             standardGeneric('simulate')
+           }
+)
+
+setMethod(
+  'simulate', 'age_model',
+  function(object, times, is_plot) {
+    #' Solves an age-structured simple SEIR model.
+    #' 
+    #' Default time series is seq(0, 100, by = 1).
+    #' This function relies on the packages deSolve and ggplot2. 
+    #' This function creates a plot of the variables over time and returns a
+    #' vectors of the incidence numbers for each age group.
+    #'
+    #' @param times (vector) time sequence over which to solve the model.
+    #'        Must be of the form seq(t_start,t_end,by=t_step).
+    #' @param is_plot (logical) whether plots for the compartments for the
+    #'        different age groups are drawn.
+    #'
+    #' @return data frame of time and outputs with incidence numbers for each
+    #'         age group.
+
+    if(!is.double(times)){
+      stop('Evaluation times of the model storage format must be a vector.')
+    }
+    if(!is.logical(is_plot)){
+      stop('Trigger for plotting must be logical.')
+    }
+    age <- object@n_age_categories
+
+    #set initial state vector
+    state <- c(S = get_parameters(object)$S0,
+               E = get_parameters(object)$E0,
+               I = get_parameters(object)$I0,
+               R = get_parameters(object)$R0)
+
+    #set parameters vector
+    parameters <- c(a = get_parameters(object)$a,
+                    b = get_parameters(object)$b,
+                    c = get_parameters(object)$c)
+
+    #function for RHS of ode system
+    right_hand_side <- function(t, state, parameters) {
+      with(
+        as.list(c(state, parameters)),
+        {
+          S <- state[1:age]
+          E <- state[(age+1):(2*age)]
+          I <- state[(2*age+1):(3*age)]
+          R <- state[(3*age+1):(4*age)]
+          # rate of change
+          dS <- -a*S*I
+          dE <- a*S*I - b*E
+          dI <- b*E - c*I
+          dR <- c*I
+          # return the rate of change
+          list(c(dS, dE, dI, dR))
+        })
+    }
+
+    #call ode solver
+    out <- ode(
+      y = state, times = times, func = right_hand_side, parms = parameters)
+    
+    output <- as.data.frame.array(out)
+
+    if(is_plot == TRUE){
+      col <- c('cS' = 'blue', 'cE' = 'green', 'cI' = 'yellow', 'cR' = 'red')
+      for(a in 1:age){
+        out_df <- output[,c(1, a+1, age+a+1, 2*age+a+1, 3*age+a+1)]
+        names(out_df) <- c('time', 'S', 'E', 'I', 'R')
+        SEIRplot <- ggplot(out_df, aes(x=time)) + 
+          geom_line(aes(y=S, color = 'cS'), size = 1.5) +
+          geom_line(aes(y=E, color = 'cE'), size = 1.5) +
+          geom_line(aes(y=I, color = 'cI'), size = 1.5) +
+          geom_line(aes(y=R, color = 'cR'), size = 1.5) +
+          labs(x = 'time', y = 'fraction of the population',
+               title = glue('SEIR model for age group {a}')) +
+          theme(legend.position = 'right') +
+          scale_color_manual(values = col)
+        print(SEIRplot)
+      }
+    }
+    
+    # compute incidence number
+    total_inf <- output[, (2*age+2):(3*age+1)] + output[, (3*age+2):(4*age+1)]
+    n_inc <- rbind(rep(0, age),
+                   total_inf[2:nrow(total_inf),]-total_inf[1:nrow(total_inf)-1,]
+                   )
+    output$Incidence <- unname(as.matrix(n_inc))
+    
+    # Return only at integer times
+    return(output)
+  })
---
+++ tests/testthat/test_age_model_class.R
@@ -0,0 +1,98 @@
+test_that("age_model gets instantiated", {
+  my_model <- new('age_model', name = 'my_model', n_age_categories = 2)
+  
+  # Test output is correct
+  expect_equal(my_model@name,
+               'my_model')
+  
+  expect_equal(my_model@output_names,
+               list('S', 'E', 'I', 'R', 'Incidence'))
+  
+  expect_equal(my_model@parameter_names,
+               list('S0', 'E0', 'I0', 'R0', 'a', 'b', 'c'))
+  
+  expect_length(my_model@parameters, 7)
+  
+  expect_equal(my_model@n_age_categories, 2)
+})
+
+test_that("can retrieve age_model parameters", {
+  my_model <- new('age_model', name = 'my_model', n_age_categories = 2)
+  
+  expect_equal(get_parameters(my_model),
+               vector(mode = "list", length = 7))
+})
+
+test_that("can set new parameters for the age_model", {
+  my_model <- new('age_model', name = 'my_model', n_age_categories = 2)
+  my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                             c(0, 0), 1, 0.5, 0.5)
+
+  # Test output is correct
+  expected_parameters <- list('S0'=c(0.4, 0.4),
+                             'E0'=c(0, 0),
+                             'I0'=c(0.05, 0.15),
+                             'R0'=c(0, 0),
+                             'a'=1,
+                             'b'=0.5,
+                             'c'=0.5)
+  
+  expect_equal(get_parameters(my_model),
+               expected_parameters)
+  
+  # Test input errors
+  expect_error({
+    my_model <- set_parameters(my_model, c(0.4, 0, 0.4), c(0, 0), c(0.05, 0.15),
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0, 0), c(0.05, 0.15),
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0, 0.15),
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                               c(0, 0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, 0, c(0, 0), c(0.05, 0.15),
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), 0, c(0.05, 0.15),
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), 0,
+                               c(0, 0), 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                               0, 1, 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                               c(0, 0), c(1, 0), 0.5, 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                               c(0, 0), 1, c(1, 0), 0.5)
+    my_model <- set_parameters(my_model, c(0.4, 0.4), c(0, 0), c(0.05, 0.15),
+                               c(0, 0), 1, 0.5, c(1, 0))
+  })
+})
+
+test_that("can run simulation for the age_model", {
+  my_model <- new('age_model', name = 'my_model', n_age_categories = 2)
+  my_model <- set_parameters(my_model, c(0.6, 0.4), c(0, 0), c(0, 0),
+                             c(0, 0), 1, 0.5, 0.5)
+
+  expected_output <- data.frame(time=0:2,
+                                S1=rep(0.6,3),
+                                S2=rep(0.4,3),
+                                E1=rep(0,3),
+                                E2=rep(0,3),
+                                I1=rep(0,3),
+                                I2=rep(0,3),
+                                R1=rep(0,3),
+                                R2=rep(0,3))
+  expected_output$Incidence <- cbind(rep(0,3), rep(0,3))
+  
+  # Test output is correct
+  expect_equal(simulate(my_model, seq(0, 2, by = 1)),
+               expected_output)
+  
+  # Test plot is called
+  
+  
+  # Test input errors
+  expect_error({
+    simulate(my_model, '0')
+    simulate(my_model, seq(0, 2, by = 1), is_plot = c(0, 0))
+  })
+})
