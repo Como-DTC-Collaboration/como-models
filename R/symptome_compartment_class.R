a6899a08a7649e8808bbd502bf8a267f18a23807
hfldai <liangti.dai@linacre.ox.ac.uk>
2021-04-10 21:37:57 +0100

    included mortality | refined docstrings (#3)

--- R/symptome_compartment_class.R
+++ R/symptome_compartment_class.R
@@ -1,48 +1,39 @@
-#' SEIR basic model for compartments of symptome
+#' An SEIR model for compartments of different symptoms
 #'
-#' model diagram:
-#'  --> S --(lam)--> E --(gamma), (ihr),((phs))--> (I, HA, VentA, ICUA) --((nu))--> R
-#'  |                                                                               |
-#'  |_____________________________________(omega)___________________________________|
+#' Note:
+#' 1) Sum of poulation equals to 1 at any time point.
+#' An error will be reported the sum of initial population is not equal to 1.
+#' 2) At this stage, we only consider disease-caused death cases
+#' calculating mortality.
 #'
-#'
-#' Population groups:
-#'
-#' S - suspeptable,
-#' E - effective,
-#' R - recovered
-#' I - infectious with mild/no symptome
-#' HA - infectious with severe symptomes that need normal hospitalization (no need for icu/vent, H+HC)
-#' VentA - infectious with severe symptomes that need venticulartor (Vent+VentC+HCV+ICUCV)
-#' ICUA - infectious with severe symptomes that need icu (ICU+ICUC+HCICU)
-#'
-#' Notice:
-#' 1) I, HA, VentA, ICUA - compartments of infectious population groups based on symptomes
-#' 2) Sum of the population at each time point are normalized to 1. 
-#' Error will be reported if the sum of initial population is not equal to 1.
-#'
-#' Parameters:
-#' lam - Numeric(range(0,1)), rate from S to E
-#' gamma - Numeric(range(0,1)),rate from E to become infectious
-#' omega - Numeric(range(0,1)),rate from R to S
-#' ihr - Numeric(range(0,1)),rate from E to infectious groups that have severe symptome (HA+VentA+ICUA)
-#' phs$phs_icua - Numeric(range(0,1)),rate from E to ICUA
-#' phs$phs_venta - Numeric(range(0,1)),rate from severe symptomes to VentA
-#' nu - Numeric vector (range(0,1)) , rate of recovery for each infectious group.
+#' model structure:
+#'    ---------------------------(omega)---------------------------------
+#'    |                                                                 |
+#'    |                                                                 |
+#'    --> S --(lam)--> E --(gamma) ------(1-ihr)-------> I ---(nu.i)--> R <--
+#'                                 |                     |                  |
+#'                                 |                 (pdeath.i)             |
+#'                                 |                     |                  |
+#'                                 |            --> CM <--                  |
+#'                                 |            |                           |
+#'                                 |      (pdeath.hsa)                      |
+#'                                 |            |                           |
+#'                                 -- (ihr)---> HA --(nu.hsa)----------------
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Class definitions
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#' @describeIn model_symptome of dependent classes
+#' @describeIn model_symptome to set up dependent classes for model_symptome
 setClass("ggplot")
 setClass("gg")
+
 #' An S4 class of an basic SEIR model with compartments of different symptomes
 #'
 #' @slot name A string gives the name of the model
 #' @slot initial_population A list of the initial population size of each group
 #' @slot parameters A list of the model parameters
 #' @slot output A dataframe holding the ode simulation output, with columns as population groups and rows as simulation time points
-#' @slot plot_output A list holding the plot of the ode simulation output
+#' @slot plot_output A ggplot holding the plot of the ode simulation output
 #'
 #' @import deSolve
 #' @import ggplot2
@@ -60,94 +51,41 @@ setClass(Class = "model_symptome",
          ),
          prototype = list(
            name = NA_character_,
-           initial_population = vector(mode = "list", length = 7) %>%
-             setNames(list("S", "E", "I", "HA", "ICUA", "VentA", "R")),
+           initial_population = vector(mode = "list", length = 6) %>%
+             setNames(list("S", "E", "I", "HA", "R", "CM")),
            parameters = vector(mode = "list", length = 6) %>%
-             setNames(list("lam", "gamma", "omega", "ihr", "phs", "nu")),
+             setNames(list("lam", "gamma", "omega", "ihr", "nu", "pdeath")),
            output = data.frame(),
            plot_output = ggplot()
          ))
-
-#' @describeIn model_symptome to check validity of the settings of the parameters and initial conditions
-#'
-#' @param object An object of class model_symptome
-#' @return TRUE or a string explaining the error message
-#'
-check_init <- function(object) {
-  errors <- character()
-  # check whether all required parameters are set
-  is_na_params <- is.na(object@parameters)
-  if (sum(is_na_params) != 0) {
-    msg <- paste("Missing parameters:",
-                 paste(names(object@parameters)[is_na_params], collapse = ", ")
-    )
-    errors <- c(errors, msg)
-  }
-
-  # check whether all required initial population groups are set
-  is_na_pop <- is.na(object@initial_population)
-  if (sum(is_na_pop) != 0) {
-    msg <- paste("Missing initial setting for population group:",
-                 paste(names(object@initial_population)[is_na_pop], collapse = ", ")
-    )
-    errors <- c(errors, msg)
-  }else{
-    # check whether the sum of initial population is normalized to 1
-    sum_init_pop <- sum(unlist(object@initial_population))
-    if (sum_init_pop != 1) {
-      msg <- "Sum of initial population is not 1, please normalize"
-      errors <- c(errors, msg)
-    }
-  }
-
-  # check whether the lengths of phs and nu are correct
-  n_phs <- length(object@parameters$phs)
-  if (n_phs != 2) {
-    msg <- paste("Length of parameter phs,", n_phs, ", is not equal to the setting ", 2)
-    errors <- c(errors, msg)
-  }
-  
-  n_nu <- length(object@parameters$nu)
-  if (n_nu != 4) {
-    msg <- paste0(
-      "Length of parameter nu,", n_nu, ", is not equal to the setting ", 4)
-    errors <- c(errors, msg)
-  }
-
-  if (length(errors) == 0) TRUE else errors
-}
-
-#' @describeIn model_symptome to set initial population sizes (in fraction) and parameters
+              
+#' @describeIn model_symptome set initial population sizes (in fraction) and parameters
 #'
 #' @param object An object of class model_symptome
 #' @param S Numeric, initial population of the susceptible
 #' @param E Numeric, initial population of the exposed
-#' @param I Numeric, initial population of the infectious with mild/no symptome
-#' @param HA Numeric, initial population of the infectious with severe symptomes 
-#' that need normal hospitalization
-#' @param ICUA Numeric, initial population of the infectious with severe symptomes that need icu
-#' @param VentA Numeric, initial population of the infectious with severe symptomes 
-#' that need venticulartor
+#' @param I Numeric, initial population of the infected with mild/no symptom
+#' @param HA Numeric, initial population of the infected with severe symptoms
+#' that need further hospitalization
+#' @param CM Numeric, initial cumulative mortality
 #' @param R Numeric, initial population of the recovered
-#' @param lam Numeric
-#' @param gamma Numeric
-#' @param omega Numeric
-#' @param ihr Numeric
-#' @param phs Numeric list
-#' @param nu Numeric list
-#' 
-#' @return An object of class model_symptome
+#' @param lam Numeric, rate at which an infected individual exposes susceptible
+#' @param gamma Numeric, rate at which exposed individuals become infected
+#' @param omega Numeric, rate at which recovered individuals become susceptible
+#' @param ihr Numeric, fraction of infected individuals develop severe symptoms
+#' @param nu Numeric vector, rate at which each infected group recover
+#' @param pdeath Numeric vector, rate of disease-caused mortality of each infected group
+#' @return An object of class model_symptome with initial population and parameters
 #'
-setGeneric(name = "set_init", 
+setGeneric(name = "set_init",
            def = function(object, S=NA_real_, E=NA_real_, I=NA_real_,
-                          HA=NA_real_, ICUA=NA_real_, VentA=NA_real_, R=NA_real_,
+                          HA=NA_real_, R=NA_real_, CM=NA_real_,
                           lam=NA_real_, gamma=NA_real_, omega=NA_real_,
-                          ihr=NA_real_, phs=list(), nu=list()) standardGeneric("set_init"))
-
+                          ihr=NA_real_, nu=list(), pdeath=list()) standardGeneric("set_init"))
 setMethod("set_init", signature(object = "model_symptome"),
-          function(object, S, E, I, HA, ICUA, VentA, R, lam, gamma, omega, ihr, phs, nu) {
-            param_list <- list(lam, gamma, omega, ihr, phs, nu)
-            init_pop_list <- list(S, E, I, HA, ICUA, VentA, R)
+          function(object, S, E, I, HA, R, CM, lam, gamma, omega, ihr, nu, pdeath) {
+            param_list <- list(lam, gamma, omega, ihr, nu, pdeath)
+            init_pop_list <- list(S, E, I, HA, R, CM)
             names(param_list) <- names(object@parameters)
             names(init_pop_list) <- names(object@initial_population)
             object@initial_population <- init_pop_list
@@ -159,15 +97,64 @@ setMethod("set_init", signature(object = "model_symptome"),
             else stop(paste(check, ", please check and rerun set_init.\n"))
           })
 
+#' @describeIn model_symptome to check validity of the settings of the parameters and initial conditions
+#'
+#' @param object An object of class model_symptome
+#' @return TRUE if passing all checks, or an error message if any check fails
+#'
+setGeneric(name = "check_init", def = function(object) standardGeneric("check_init"))
+setMethod("check_init", signature(object = "model_symptome"),
+          function(object) {
+            errors <- character()
+            # check whether all required parameters are set
+            is_na_params <- is.na(object@parameters)
+            if (sum(is_na_params) != 0) {
+              msg <- paste("Missing parameters:",
+                           paste(names(object@parameters)[is_na_params], collapse = ", ")
+              )
+              errors <- c(errors, msg)
+            }
+            # check whether all required initial population groups are set
+            is_na_pop <- is.na(object@initial_population)
+            if (sum(is_na_pop) != 0) {
+              msg <- paste("Missing initial setting for population group:",
+                           paste(names(object@initial_population)[is_na_pop], collapse = ", ")
+              )
+              errors <- c(errors, msg)
+            }else{
+              # check whether the sum of initial population is normalized to 1
+              sum_init_pop <- sum(unlist(object@initial_population))
+              if (sum_init_pop != 1) {
+                msg <- "Sum of initial population is not 1, please normalize"
+                errors <- c(errors, msg)
+              }
+            }
+            # check whether the lengths of nu and pdeath correspond to the number of infected groups
+            n_nu <- length(object@parameters$nu)
+            if (n_nu != 2) {
+              msg <- paste0(
+                "Length of parameter nu,", n_nu, ", is not equal to the setting ", 2)
+              errors <- c(errors, msg)
+            }
+            n_pdeath <- length(object@parameters$pdeath)
+            if (n_pdeath != 2) {
+              msg <- paste0(
+                "Length of parameter pdeath,", n_pdeath, ", is not equal to the setting ", 2)
+              errors <- c(errors, msg)
+            }
+            if (length(errors) == 0) TRUE else errors
+          })
+
+
 #' @describeIn model_symptome to solve the ode system.
-#' Notice that the population is normalized to 1 for each time point.
-#' 
+#'
 #' @param object An object of class model_symptome
 #' @param times A list of time points of the simulation period
-#' @param method A string indicating which ode integrator to use. Default is set to 'lsoda'.
-#' @return An object of class model_symptome
+#' @param method A string indicating which ode integrator to use. Default is set to 'lsoda'
+#' @return An object of class model_symptome with a dataframe of simulation output
 #'
-setGeneric(name = "ode_simulate", def = function(object, times, method = "lsoda") standardGeneric("ode_simulate"))
+setGeneric(name = "ode_simulate",
+           def = function(object, times, method = "lsoda") standardGeneric("ode_simulate"))
 
 setMethod("ode_simulate", signature(object = "model_symptome"),
   function(object, times, method) {
@@ -176,71 +163,61 @@ setMethod("ode_simulate", signature(object = "model_symptome"),
                     E = object@initial_population$E,
                     I = object@initial_population$I,
                     HA = object@initial_population$HA,
-                    ICUA = object@initial_population$ICUA,
-                    VentA = object@initial_population$VentA,
-                    R = object@initial_population$R)
+                    R = object@initial_population$R,
+                    CM = object@initial_population$CM)
     # parameters
     params <- c(lam = object@parameters$lam,
                 gamma = object@parameters$gamma,
                 omega = object@parameters$omega,
                 ihr = object@parameters$ihr,
-                phs = object@parameters$phs,
-                nu = object@parameters$nu)
-  
+                nu = object@parameters$nu,
+                pdeath = object@parameters$pdeath)
+
     # ODE system RHS
     ode_symptome_rhs <- function(t, pop_groups, parameters) {
       with(
-        as.list(c(pop_groups, params)),
-        {
-          dSdt <- -lam * S * (I + HA + ICUA + VentA) + omega * R
-          dEdt <- lam * S * (I + HA + ICUA + VentA) - gamma * E
-          dIdt <- -nu.nui * I + (1.0 - ihr) * gamma * E # - ratetestI*I
-          dHAdt <- gamma * ihr * (1 - phs.phs_icua - phs.phs_venta) * E - nu.nuhsa * HA
-          dICUAdt <- gamma * ihr * phs.phs_icua * E - nu.nu_icua * ICUA
-          dVentAdt <- gamma * ihr * phs.phs_venta * E - nu.nu_venta * VentA
-          dRdt <- -omega * R + nu.nui * I + nu.nuhsa * HA + nu.nu_icua * ICUA + nu.nu_venta * VentA
+        as.list(c(pop_groups, params)), {
+          dS <- -lam * S * (I + HA) + omega * R
+          dE <- lam * S * (I + HA) - gamma * E
+          dI <- (1.0 - ihr) * gamma * E - nu.i * I  - pdeath.i * I
+          dHA <- ihr * gamma * E - nu.hsa * HA  - pdeath.hsa * HA
+          dR <- -omega * R + nu.i * I + nu.hsa * HA
+          dCM <-  pdeath.i * I + pdeath.hsa * HA
           # return the rate of change
-          list(c(dSdt, dEdt, dIdt, dHAdt, dICUAdt, dVentAdt, dRdt))
+          list(c(dS, dE, dI, dHA, dR, dCM))
         })
     }
     # solving ode
-    output <- ode(y = pop_groups, times = times, func = ode_symptome_rhs, parms = params, method = method)
+    output <- ode(y = pop_groups,
+                  times = times,
+                  func = ode_symptome_rhs,
+                  parms = params,
+                  method = method)
     output <- as.data.frame(output)
-
-    # normalize: keep the output population as fraction (range(0,1)) for each time step
-    output.pop <- output[, -which(names(output) == "time")]
-    output.pop <- sweep(output.pop, 1, apply(output.pop, 1, min))
-    output[, -which(names(output) == "time")] <- output.pop / rowSums(output.pop)
     object@output <- output
     return(object)
     })
 
 #' @describeIn model_symptome to plot the outcome of the ode similuation
-#' - the population change of each group by time.
 #'
 #' @param object An object of class model_symptome
-#' @return An object of class model_symptome or a string of error message
+#' @return An object of class model_symptome with a plot showing the ode simulation outcome
 #'
-setGeneric(name = "plot_ode_output", def = function(object) standardGeneric("plot_ode_output"))
-
+setGeneric(name = "plot_ode_output",
+           def = function(object) standardGeneric("plot_ode_output"))
 setMethod("plot_ode_output", signature(object = "model_symptome"),
           function(object) {
-            # if(is_analytical){
-            #   output <- object@output_analytical
-            # }else{
-            #   output <- object@output
-            # }
             output <- object@output
             if (empty(output)) {
             error <- "Empty output. \
-            Please run the ode solver by calling ode_simulate."
+            Please first run the ode simulation by calling ode_simulate."
             return(error)
             }else{
               # reshape data frame
               output.melt <- melt(output, id.vars = "time")
               names(output.melt) <- c("time", "population_group", "fraction")
               # plot the each group
-              p <- ggplot2::ggplot(data = output.melt, aes(x = time, y = fraction)) +
+              p <- ggplot(data = output.melt, aes(x = time, y = fraction)) +
                 geom_line(aes(colour = population_group)) +
                 theme_classic()
               object@plot_output <- p
--- tests/testthat/test_symptome_compartment_class.R
+++ tests/testthat/test_symptome_compartment_class.R
@@ -1,27 +1,24 @@
 # Tests for functions in symptome_compartment_class.R on simulated test data
 # --------------------------------------------------------------------------
-# load test data
-# load("../testdata/testdata_symptome_compartment_class.RData")
 
 # set up test data:
 # initial population (in fraction)
-S = 1-1e-7
-E = 1e-7
-I = 0
-HA = 0
-VentA = 0
-ICUA = 0
-R = 0
+S <- 0.9
+E <- 0.1
+I <- 0
+HA <- 0
+R <- 0
+CM <- 0
 
 # params
-lam = 1
-gamma = 1
-omega = 0.01
-ihr = 0.1
-phs = list(phs_icua=0.1, phs_venta=0.05)
-nu = list(nui=0.5, nuhsa=0.1, nu_venta=0.005, nu_icua=0.01) 
-# simulation period: 200 time points (to relaxation)
-t <- seq(0, 200, by = 1)
+lam <- 1.0
+gamma <- 0.5
+omega <- 0.01
+ihr <- 0.1
+nu <- list(i = 0.5, hsa = 0.05)
+pdeath <- list(i = 0.05, hsa = 0.3)
+# simulation period: 3000 time points (to relaxation)
+t <- seq(0, 3000, by = 1)
 
 # test: validate model settings - ode simulation - plot, on test data
 test_that("model symptome", {
@@ -29,9 +26,9 @@ test_that("model symptome", {
   model <- new("model_symptome", name = "model")
   # 2. set up parameters and initial population
   model <- set_init(object = model, S = S, E = E, I = I,
-                    HA = HA, VentA = VentA, ICUA = ICUA, R = R,
+                    HA = HA, R = R, CM = CM,
                     lam = lam, gamma = gamma, omega = omega,
-                    ihr = ihr, phs = phs, nu = nu)
+                    ihr = ihr, nu = nu, pdeath = pdeath)
   # 3. ode simulation and plot
   model <- ode_simulate(model, t)
   model <- plot_ode_output(model)
@@ -44,10 +41,11 @@ test_that("model symptome", {
   # check @output
   expect_equal(dim(model@output),
                c(length(t), length(model@initial_population) + 1))
-  expect_equal(colnames(model@output), c("time", "S", "E", "I", "HA", "ICUA", "VentA", "R"))
+  expect_equal(colnames(model@output),
+               c("time", "S", "E", "I", "HA", "R", "CM"))
   # check whether the sum of population at each simulation time point == 1
   expect_equal(rowSums(model@output[, -which(names(model@output) == "time")]),
-               rep(1.0, length(t)))
+                rep(1.0, length(t)))
   # check @plot_output
   expect_s3_class(model@plot_output, "ggplot")
 })
@@ -58,18 +56,18 @@ test_that("model error input", {
   expect_error({
     # initial population sum not equal to 1
     set_init(object = model, S = 0.1, E = E, I = I,
-             HA = HA, VentA = VentA, ICUA = ICUA, R = R,
+             HA = HA, R = R, CM = CM,
              lam = lam, gamma = gamma, omega = omega,
-             ihr = ihr, phs = phs, nu = nu)
+             ihr = ihr, nu = nu, pdeath = pdeath)
     # missing population group
     set_init(object = model, E = E, I = I,
-             HA = HA, VentA = VentA, ICUA = ICUA, R = R,
+             HA = HA, R = R, CM = CM,
              lam = lam, gamma = gamma, omega = omega,
-             ihr = ihr, phs = phs, nu = nu)
+             ihr = ihr, nu = nu, pdeath = pdeath)
     # missing parameter
     set_init(object = model, S = S, E = E, I = I,
-             HA = HA, VentA = VentA, ICUA = ICUA, R = R,
+             HA = HA, R = R, CM = CM,
              gamma = gamma, omega = omega,
-             ihr = ihr, phs = phs, nu = nu)
+             ihr = ihr, nu = nu, pdeath = pdeath)
     })
 })
