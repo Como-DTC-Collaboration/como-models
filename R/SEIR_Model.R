7cd3c9a769b2e926a167341a59a8a72f3ef0fb9d
FarmHJ <huijia-farm_95@hotmail.com>
2021-04-15 23:42:12 +0800

    Separate parameters, setter getter method, include incidence

--- R/SEIR_Model.R
+++ R/SEIR_Model.R
@@ -3,134 +3,195 @@
 #' This class represents the SEIR model, showing how populations of susceptible,
 #' exposed, infectious and recovered individuals evolve over time.
 #'
-#' @slot name character representing name of model
-#' @slot parameter_names list of names of parameters (characters). Default is
-#'       list("S0", "E0", "I0", "R0", "b", "k", "g")
-#' @slot parameters list of parameter values (double). Default is a list of 1s.
+#' @slot output_names list of compartments name which are used by the model and
+#'       incidence.
+#' @slot initial_condition_names list of names of initial conditions
+#'       (characters). Default is list("S0", "E0", "I0", "R0").
+#' @slot transmission_parameter_names list of names of transmission parameters
+#'       (characters). Default is list("b", "k", "g").
+#' @slot initial_conditions list of values for initial conditions(double). 
+#' @slot transmission_parameters list of values for transmission parameters
+#'       (double).
 #'
 #' @import deSolve
 #' @import glue
-#' @import ggplot2
 
 setClass("SEIR_model",
          # slots
          slots = c(
-           name = "character",
-           parameter_names = "list",
-           parameters = "list"
+           output_names = 'list',
+           initial_condition_names = 'list',
+           transmission_parameter_names = 'list',
+           initial_conditions = 'list',
+           transmission_parameters = 'list'
          ),
          # prototypes for the slots, automatically set parameter names and
-         # values
+         # its data type
          prototype = list(
-           name = NA_character_,
-           parameter_names = list("S0", "E0", "I0", "R0", "b", "k", "g"),
-           parameters = as.list(rep(1, 7))
+           output_names = list("S", "E", "I", "R", "Incidence"),
+           initial_condition_names = list("S0", "E0", "I0", "R0"),
+           transmission_parameter_names = list("b", "k", "g"),
+           initial_conditions = vector(mode = "list", length = 4),
+           transmission_parameters = vector(mode = "list", length = 3)
          )
 )
 
-#' @describeIn SEIR_model prints the parameters of the SEIR model in object.
+#' @describeIn SEIR_model retrieves initial conditions for SEIR model.
 #'
 #' @param object An object of the class SEIR_model.
-#' @export
+setGeneric("initial_conditions",
+           function(object) standardGeneric("initial_conditions"))
 
-setGeneric("get_parameters", function(object) standardGeneric("get_parameters"))
-setMethod("get_parameters", "SEIR_model", function(object) object@parameters)
+setMethod("initial_conditions", "SEIR_model",
+          function(object) object@initial_conditions)
 
+#' @describeIn SEIR_model retrieves transmission parameters for SEIR model.
+#'
+#' @param object An object of the class SEIR_model.
+setGeneric("transmission_parameters",
+           function(object) standardGeneric("transmission_parameters"))
 
-#' @describeIn SEIR_model sets the parameter values (initial conditions S0, E0,
-#' I0, R0, and parameters b, k, g) of the SEIR model contained in object.
+setMethod("transmission_parameters", "SEIR_model",
+          function(object) object@transmission_parameters)
+
+#' @describeIn SEIR_model setter and getter method for initial conditions (S0, 
+#' E0, I0 and R0) of the SEIR model.
+#'
+#' If the initial conditions provided to do not sum to 1, an error is thrown.
 #'
 #' @param object an object of the class SEIR_model
 #' @param S0 (double) initial fraction of the population that is susceptible
 #' @param E0 (double) initial fraction of the population that has been exposed
 #' @param I0 (double) initial fraction of the population that is infectious
-#' @param R0 (double) initial fraction of the population that has recovered
+#' @param R0 (double) initial fraction of the population that has recovered#' All initial conditions must sum up to 1.
+#'
+#' @return object of class SEIR_model with initial conditions assigned.
+#' @export
+
+setGeneric(
+  "initial_conditions<-",
+  function(object, S0, E0, I0, R0){
+    standardGeneric("initial_conditions<-")
+  })
+
+setMethod(
+  "initial_conditions<-", "SEIR_model",
+  function(object, S0, E0, I0, R0) {
+    
+    # create list of parameter values
+    init_cond <- list(S0, E0, I0, R0)
+    
+    # add names to each value
+    names(init_cond) = object@initial_condition_names
+    
+    # raise errors if age category dimensions do not match initial state vectors
+    # also raise errors if initial state and parameter values are not doubles
+    for (p in list("S0", "E0", "I0", "R0")){
+      if(!is.numeric(init_cond[[p]])){
+        stop(glue("{p} format must be numeric"))}
+    }
+    
+    # check that the initial conditions are properly normalized
+    if (sum(S0, E0, I0, R0) != 1) {
+      stop("Invalid initial conditions. Must add up to 1.")
+    }
+    
+    # if all above tests are passed, assign the init_cond namelist to the object
+    object@initial_conditions <- init_cond
+    
+    return(object)
+  })
+
+#' @describeIn SEIR_model setter and getter method for transmission parameters 
+#' (b, k and g) of the SEIR model.
+#'
+#' If the transmission parameters provided to are not 1-dimensional an error is
+#' thrown.
+#' 
 #' @param b (double) rate at which an infected individual exposes susceptible
 #' @param k (double) rate at which exposed individuals become infectious
 #' @param g (double) rate at which infected individuals recover
 #'
-#' @return object of class SEIR_model with initial conditions and parameter
-#' values assigned.
+#' @return object of class SEIR_model with transmission parameter values
+#' assigned.
 #' @export
 
 setGeneric(
-  "set_parameters",
-  function(object, S0, E0, I0, R0, b, k, g) {
-    standardGeneric("set_parameters")
+  "transmission_parameters<-",
+  function(object, b, k, g){
+    standardGeneric("transmission_parameters<-")
   })
+
 setMethod(
-  "set_parameters", "SEIR_model",
-  function(object, S0, E0, I0, R0, b, k, g) {
-
+  "transmission_parameters<-", "SEIR_model",
+  function(object, b, k, g) {
+    
     # create list of parameter values
-    params <- list(S0, E0, I0, R0, b, k, g)
-
+    trans_params <- list(b, k, g)
+    
     # add names to each value
-    names(params) <- object@parameter_names
-
-    # raise errors if initial state and parameter values are not doubles
-     for (p in list("S0", "E0", "I0", "R0")) {
-      if (!is.double(params[[p]])) {
-        stop(glue("{p} must be of type double."))
-        }
+    names(trans_params) = object@transmission_parameter_names
+    
+    # check format of parameters b, k and g
+    if(length(b) != 1 | length(k) != 1 | length(g) != 1){
+      stop("The parameter values should be 1-dimensional.")
     }
-
-    # check format of parameters b, k, and g
-    if (any(length(b) != 1 | length(k) != 1 | length(g) != 1)) {
-      stop("The rates of change between compartments are 1-dimensional.")
-    }
-
-    # check that the initial conditions are properly normalized
-    if (S0 + E0 + I0 + R0 != 1) {
-      stop("Initial conditions do not add up to 1.")
-    }
-
-    #if all above tests are passed, assign the params namelist to the object
-    object@parameters <- params
-
+    
+    # if all above tests are passed, assign the trans_params namelist to the
+    # object
+    object@transmission_parameters <- trans_params
+    
     return(object)
   })
 
 # SEIR_model class specific functions
 
-#' @describeIn SEIR_model solves the ODEs of the SEIR_model specified in object
-#' for the time points specified in times.
+#' @describeIn SEIR_model solves ODEs of the SEIR_model specified in object
+#' for the time points specified in times and integration method specified in 
+#' solve_method.
+#'
+#' \deqn{\frac{dS(t)}{dt} = - b S(t) I(t)
+#' \deqn{\frac{dE(t)}{dt} =  b S(t) I(t) - k E(t)
+#' \deqn{\frac{dI(t)}{dt} = k E(t) - g I(t)
+#' \deqn{\frac{dR(t)}{dt} = g I(t)
+#'
+#' This function relies on the package deSolve.
 #'
 #' @param object an object of the class SEIR_model
 #' @param times (double) a sequence of time points at which the solution to
-#' the system of ODEs should be returned.
-#' @param is_plot (logical) indicates whether the solutions to the ODEs will
-#' be plotted. Default is TRUE.
+#' the system of ODEs should be returned. Must be of the form 
+#' seq(t_start, t_end, by=t_step). Default time series is seq(0, 100, by = 1).
+#' @param solve_method (string) a string of chosen numerical integration method 
+#' for solving the ode system. Default is "lsoda" which is also the default for 
+#' the ode function in the deSolve package used in this function.
 #'
-#' @return a dataframe with the time steps, and solutions for S, E, I and R,
-#' and a plot if is_plot is TRUE.
+#' @return a dataframe with the time steps, time series of S, E, I and R
+#' population fractions and incidence numbers of the SEIR model.
 #' @export
 
-setGeneric(name = "simulate",
-           def = function(object, times = seq(0, 100, by = 1), is_plot = TRUE) {
-             standardGeneric("simulate")
+setGeneric(name = "simulate_SEIR",
+           def = function(object, times = seq(0, 100, by = 1),
+                          solve_method = "lsoda") {
+             standardGeneric("simulate_SEIR")
            }
 )
+
 setMethod(
-  "simulate", "SEIR_model",
-  function(object, times, is_plot) {
+  "simulate_SEIR", "SEIR_model",
+  function(object, times, solve_method = "lsoda") {
     if (!is.double(times)) {
       stop("Evaluation times of the model storage format must be a vector.")
     }
 
-    if (!is.logical(is_plot)) {
-      stop("is_plot must be of type logical.")
-    }
-
     # set initial state vector
-    state <- c(S = get_parameters(object)$S0,
-               E = get_parameters(object)$E0,
-               I = get_parameters(object)$I0,
-               R = get_parameters(object)$R0)
-    # set parameters vector
-    parameters <- c(b = get_parameters(object)$b,
-                    k = get_parameters(object)$k,
-                    g = get_parameters(object)$g)
+    state <- c(S = initial_conditions(object)$S0,
+               E = initial_conditions(object)$E0,
+               I = initial_conditions(object)$I0,
+               R = initial_conditions(object)$R0)
+    # set transmission parameters vector
+    parameters <- c(b = transmission_parameters(object)$b,
+                    k = transmission_parameters(object)$k,
+                    g = transmission_parameters(object)$g)
     # function for RHS of ode system
     right_hand_side <- function(t, state, parameters) {
       with(
@@ -151,24 +212,17 @@ setMethod(
 
     # call ode solver
     out <- ode(
-      y = state, times = times, func = right_hand_side, parms = parameters)
+      y = state, times = times, func = right_hand_side,
+      parms = parameters, method = solve_method)
 
-    output <- as.data.frame(out)
+    # output as a dataframe
+    output <- as.data.frame.array(out)
+    
+    # compute incidence number
+    total_inf <- out_df$I + out_df$R
+    n_inc <- c(0,total_inf[2:length(total_inf)]-total_inf[1:length(total_inf)-1])
 
-    if (is_plot == TRUE) {
-      col <- c("cS" = "blue", "cE" = "green", "cI" = "yellow", "cR" = "red")
-      SEIRplot <- ggplot(output, aes(x = time)) +
-          geom_line(aes(y = S, color = "cS"), size = 1.5) +
-          geom_line(aes(y = E, color = "cE"), size = 1.5) +
-          geom_line(aes(y = I, color = "cI"), size = 1.5) +
-          geom_line(aes(y = R, color = "cR"), size = 1.5) +
-          labs(x = "time", y = "fraction of the population",
-               title = glue("SEIR model")) +
-          theme(legend.position = "right") +
-          scale_color_manual(values = col)
-        #show plot
-        print(SEIRplot)
-    }
-
+    output$Incidence <- unname(as.matrix(n_inc))
+    
     return(output)
   })
