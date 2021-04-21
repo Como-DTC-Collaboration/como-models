66848549e3c07a4787b5412e47a785b177b160bd
solveigvandervegt <sav57@cornell.edu>
2021-04-21 09:54:08 +0100

    Cleaned up documentation
    Deleted double documentation of methods and generics

--- R/SEIDR.R
+++ R/SEIDR.R
@@ -52,11 +52,9 @@ setClass("SEIDR",
 setGeneric("initial_conditions",
            function(object) standardGeneric("initial_conditions"))
 
-#' Retrieves initial conditions for SEIDR model
-#' 
+
 #' @rdname initial_conditions-method
 #' @aliases initial_conditions,ANY,ANY-method
-#' @param object An object of the class SEIDR.
 #' @export
 
 setMethod("initial_conditions", "SEIDR",
@@ -71,9 +69,6 @@ setMethod("initial_conditions", "SEIDR",
 setGeneric("initial_cases_deaths",
            function(object) standardGeneric("initial_cases_deaths"))
 
-#' Retrieves initial cases and deaths for SEIDR model.
-#'
-#' @param object An object of the class SEIDR.
 #' @rdname initial_cases_deaths-method
 #' @aliases initial_cases_deaths,ANY,ANY-method
 #' @export
@@ -90,9 +85,6 @@ setMethod("initial_cases_deaths", "SEIDR",
 setGeneric("transmission_parameters",
            function(object) standardGeneric("transmission_parameters"))
 
-#' Retrieves transmission parameters for SEIR model.
-#'
-#' @param object An object of the class SEIDR.
 #' @rdname transmission_parameters-method
 #' @aliases transmission_parameters,ANY,ANY-method
 #' @export
@@ -119,16 +111,6 @@ setGeneric(
     standardGeneric("initial_conditions<-")
   })
 
-#' Setter method for initial conditions (S0, E0, I0 and R0) of the SEIR model.
-#'
-#' All initial conditions must sum up to 1.
-#' If the initial conditions provided to do not sum to 1, an error is thrown.
-#'
-#' @param object an object of the class SEIDR
-#' @param value (list) list of initial conditions S0, E0, I0, R0.
-#' 
-#' @return object of class SEIDR with initial conditions assigned.
-#' 
 #' @rdname initial_conditions-method
 #' @aliases initial_conditions<-,ANY,ANY-method
 #' @export
@@ -187,17 +169,7 @@ setGeneric(
     standardGeneric("transmission_parameters<-")
   })
 
-#' Setter method for transmission parameters
-#' (b, k, g and m) of the SEIR model.
-#'
-#' If the transmission parameters provided to are not 1-dimensional an error is
-#' thrown.
-#'
-#' @param object (SEIDR model)
-#' @param value (list) list of values for b, k, g, m, respectively.
-#'
-#' @return object of class SEIDR with transmission parameter values
-#' assigned.
+
 #' @rdname transmission_parameters-method
 #' @aliases transmission_parameters<-,ANY,ANY-method
 #' @export
@@ -260,29 +232,9 @@ setGeneric(name = "simulate_SEIDR",
                           solve_method = "lsoda") {
              standardGeneric("simulate_SEIDR")})
 
-#' Solves ODEs of the SEIDR specified in object
-#' for the time points specified in times and integration method specified in
-#' solve_method.
-#'
-#' \deqn{\frac{dS(t)}{dt} = - b S(t) I(t)}
-#' \deqn{\frac{dE(t)}{dt} =  b S(t) I(t) - k E(t)}
-#' \deqn{\frac{dI(t)}{dt} = k E(t) - (g + m) I(t)}
-#' \deqn{\frac{dR(t)}{dt} = g I(t)}
-#' \deqn{\frac{dC(t)}{dt} = b S(t) I(t)}
-#' \deqn{\frac{dD(t)}{dt} = m I(t)}
-#'
-#' This function relies on the package deSolve.
-#'
-#' @param object an object of the class SEIDR
-#' @param times (double) a sequence of time points at which the solution to
-#' the system of ODEs should be returned. Must be of the form
-#' seq(t_start, t_end, by=t_step). Default time series is seq(0, 100, by = 1).
-#' @param solve_method (string) a string of chosen numerical integration method
-#' for solving the ode system. Default is "lsoda" which is also the default for
-#' the ode function in the deSolve package used in this function.
-#'
-#' @return a dataframe with the time steps, time series of S, E, I and R
-#' population fractions, and incidence numbers and deaths of the SEIDR model.
+#' @describeIn SEIDR Simulate the SEIDR model defined in object over the
+#' time sequence defined in times, with the solver solve_method (default lsoda)
+#' 
 #' @rdname simulate_SEIDR-method
 #' @aliases simulate_SEIDR,ANY,ANY-method
 #' @export
