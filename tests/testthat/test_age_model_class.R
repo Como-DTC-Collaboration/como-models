73d01a51a4369ca9593fe1a955602dc5fa90b7a7
Oscar Dimdore-Miles <32623897+oscardm20994@users.noreply.github.com>
2021-06-10 10:20:13 +0100

    added death compartment

--- tests/testthat/test_age_model_class.R
+++ tests/testthat/test_age_model_class.R
@@ -8,17 +8,17 @@ test_that("SEIRAge gets instantiated", {
                list('0-50', '50-100'))
 
   expect_equal(my_model@output_names,
-               list('S', 'E', 'I', 'R', 'Incidence'))
+               list('S', 'E', 'I', 'R', 'D', 'Incidence'))
 
   expect_equal(my_model@initial_condition_names,
-               list('S0', 'E0', 'I0', 'R0'))
+               list('S0', 'E0', 'I0', 'R0', 'D0'))
 
   expect_equal(my_model@transmission_parameter_names,
-               list('b', 'k', 'g'))
+               list('b', 'k', 'g', 'mu'))
 
-  expect_length(my_model@initial_conditions, 4)
+  expect_length(my_model@initial_conditions, 5)
 
-  expect_length(my_model@transmission_parameters, 3)
+  expect_length(my_model@transmission_parameters, 4)
 
   expect_length(my_model@age_ranges, my_model@n_age_categories)
 
@@ -29,14 +29,14 @@ test_that("can retrieve SEIRAge initial conditions", {
   my_model <- SEIRAge(n_age_categories = 2)
 
   expect_equal(initial_conditions(my_model),
-               vector(mode = "list", length = 4))
+               vector(mode = "list", length = 5))
 })
 
 test_that("can retrieve SEIRAge transmission parameters", {
   my_model <- SEIRAge(n_age_categories = 2)
 
   expect_equal(transmission_parameters(my_model),
-               vector(mode = "list", length = 3))
+               vector(mode = "list", length = 4))
 })
 
 test_that("can set new initial conditions for the SEIRAge", {
@@ -44,13 +44,15 @@ test_that("can set new initial conditions for the SEIRAge", {
   initial_conditions(my_model)<-list(S0=c(0.4, 0.4),
                                      E0=c(0, 0),
                                      I0=c(0.05, 0.15),
-                                     R0=c(0, 0))
+                                     R0=c(0, 0), 
+                                     D0 = c(0, 0))
 
   # Test output is correct
   expected_ics <- list('S0'=c(0.4, 0.4),
                        'E0'=c(0, 0),
                        'I0'=c(0.05, 0.15),
-                       'R0'=c(0, 0))
+                       'R0'=c(0, 0),
+                       'D0' = c(0, 0))
 
   expect_equal(initial_conditions(my_model),
                expected_ics)
@@ -60,78 +62,96 @@ test_that("can set new initial conditions for the SEIRAge", {
     initial_conditions(my_model) <- list(S0=c(0.4, 0, 0.4),
                                          E0=c(0, 0),
                                          I0=c(0.5, 0.15),
-                                         R0=c(0, 0)))
+                                         R0=c(0, 0),                                   
+                                         D0 = c(0, 0))
+    )
   expect_error(
     initial_conditions(my_model) <- list(S0=c(0.4, 0, 0.4),
                                          E0=c(0, 0),
                                          I0=c(0.05, 0.15),
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
   )
   expect_error(
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=c(0, 0, 0),
                                          I0=c(0.05, 0.15),
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
+    
   )
    expect_error( 
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=c(0, 0),
                                          I0=c(0.05, 0, 0.15),
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
+    
    )
    expect_error(
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=c(0, 0),
                                          I0=c(0.05, 0.15),
-                                         R0=c(0, 0, 0))
+                                         R0=c(0, 0, 0),
+                                         D0 = c(0, 0))
+   
    )
     expect_error( 
     initial_conditions(my_model) <- list(S0=0,
                                          E0=c(0, 0),
                                          I0=c(0.05, 0.15),
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
+    
     )
     expect_error( 
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=0,
                                          I0=c(0.05, 0.15),
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
+    
     )
     expect_error( 
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=c(0, 0),
                                          I0=0,
-                                         R0=c(0, 0))
+                                         R0=c(0, 0),
+                                         D0 = c(0, 0))
+    
     )
     expect_error( 
     initial_conditions(my_model) <- list(S0=c(0.4, 0.4),
                                          E0=c(0, 0),
                                          I0=c(0.05, 0.15),
-                                         R0=0)
+                                         R0=0,
+                                         D0 = c(0, 0))
+    
     )
 })
 
 test_that("can set new transmission parameters for the SEIRAge", {
   my_model <- SEIRAge(n_age_categories = 2)
-  transmission_parameters(my_model) <- list(b=1, k=0.5, g=0.5)
+  transmission_parameters(my_model) <- list(b=1, k=0.5, g=0.5, mu = 0.01)
 
   # Test output is correct
   expected_transpar <- list('b'=1,
                             'k'=0.5,
-                            'g'=0.5)
+                            'g'=0.5,
+                            'mu' = 0.01)
 
   expect_equal(transmission_parameters(my_model),
                expected_transpar)
 
   # Test input errors
   expect_error(
-    transmission_parameters(my_model) <- list(b=c(1, 0), k=0.5, g=0.5)
+    transmission_parameters(my_model) <- list(b=c(1, 0), k=0.5, g=0.5, mu = 0.01)
   )
   expect_error(
-    transmission_parameters(my_model) <- list(b=1, k=c(1, 0), g=0.5)
+    transmission_parameters(my_model) <- list(b=1, k=c(1, 0), g=0.5, mu = 0.01)
   )
   expect_error(
-    transmission_parameters(my_model) <- list(b=1, k=0.5, g=c(1, 0))
+    transmission_parameters(my_model) <- list(b=1, k=0.5, g=c(1, 0),  mu = 0.01)
   )
 })
 
@@ -143,23 +163,26 @@ test_that("can run simulation for the SEIRAge", {
   initial_conditions(my_model) <- list(S0=c(0.6, 0.4),
                                        E0=c(0, 0),
                                        I0=c(0, 0),
-                                       R0=c(0, 0))
+                                       R0=c(0, 0),
+                                       D0 = c(0,0))
 
-  transmission_parameters(my_model) <- list(b=1, k=0.5, g=0.5)
+  transmission_parameters(my_model) <- list(b=1, k=0.5, g=0.5, mu = 0.01)
 
-  expected_output <- data.frame('time'=rep(0:2, 10),
-                                'value'=c(rep(0.6, 3), rep(0.4, 3), rep(0, 24)))
+  expected_output <- data.frame('time'=rep(0:2, 12),
+                                'value'=c(rep(0.6, 3), rep(0.4, 3), rep(0, 30)))
                                 
   times = seq(0, 2, by = 1)
   expected_output$compartment = c(replicate(length(times)*2, "S"),
                            replicate(length(times)*2, "E"),
                            replicate(length(times)*2, "I"),
                            replicate(length(times)*2, "R"),
+                           replicate(length(times)*2, "D"),
                            replicate(length(times)*2, "Incidence"))
   expected_output$age_range = unlist(rep(my_model@age_ranges, each=3))
   expected_output <- expected_output %>% 
     dplyr::mutate(compartment=as.factor(compartment)) %>% 
-    dplyr::mutate(compartment=forcats::fct_relevel(compartment, "S", "E", "I", "R")) %>% 
+    dplyr::mutate(compartment=forcats::fct_relevel(compartment, "S", "E", "I",
+                                                   "R", "D")) %>% 
     dplyr::mutate(age_range=as.factor(age_range)) %>% 
     dplyr::mutate(age_range=forcats::fct_relevel(age_range, my_model@age_ranges))
   
