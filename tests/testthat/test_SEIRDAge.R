595a43818906c1a11bc27fccde5032469e8f311a
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-06-25 15:32:24 +0100

    Rename classes and files

--- tests/testthat/test_SEIRDAge.R
+++ tests/testthat/test_SEIRDAge.R
@@ -1,5 +1,5 @@
-test_that("SEIRAge gets instantiated", {
-  my_model <- SEIRAge(n_age_categories = 2,
+test_that("SEIRDAge gets instantiated", {
+  my_model <- SEIRDAge(n_age_categories = 2,
                       age_ranges = list('0-50', '50-100'))
 
   # Test output is correct
@@ -25,22 +25,22 @@ test_that("SEIRAge gets instantiated", {
   expect_equal(my_model@n_age_categories, 2)
 })
 
-test_that("can retrieve SEIRAge initial conditions", {
-  my_model <- SEIRAge(n_age_categories = 2)
+test_that("can retrieve SEIRDAge initial conditions", {
+  my_model <- SEIRDAge(n_age_categories = 2)
 
   expect_equal(initial_conditions(my_model),
                vector(mode = "list", length = 5))
 })
 
-test_that("can retrieve SEIRAge transmission parameters", {
-  my_model <- SEIRAge(n_age_categories = 2)
+test_that("can retrieve SEIRDAge transmission parameters", {
+  my_model <- SEIRDAge(n_age_categories = 2)
 
   expect_equal(transmission_parameters(my_model),
                vector(mode = "list", length = 4))
 })
 
-test_that("can set new initial conditions for the SEIRAge", {
-  my_model <- SEIRAge(n_age_categories = 2)
+test_that("can set new initial conditions for the SEIRDAge", {
+  my_model <- SEIRDAge(n_age_categories = 2)
   initial_conditions(my_model)<-list(S0=c(0.4, 0.4),
                                      E0=c(0, 0),
                                      I0=c(0.05, 0.15),
@@ -130,8 +130,8 @@ test_that("can set new initial conditions for the SEIRAge", {
     )
 })
 
-test_that("can set new transmission parameters for the SEIRAge", {
-  my_model <- SEIRAge(n_age_categories = 2)
+test_that("can set new transmission parameters for the SEIRDAge", {
+  my_model <- SEIRDAge(n_age_categories = 2)
   transmission_parameters(my_model) <- list(b=1, k=0.5, g=0.5, mu = 0.01)
 
   # Test output is correct
@@ -155,8 +155,8 @@ test_that("can set new transmission parameters for the SEIRAge", {
   )
 })
 
-test_that("can run simulation for the SEIRAge", {
-  my_model <- SEIRAge(n_age_categories = 2,
+test_that("can run simulation for the SEIRDAge", {
+  my_model <- SEIRDAge(n_age_categories = 2,
                       age_ranges = list('0-50', '50-100'), 
                       contact_matrix = matrix(c(1,0,0,1), nrow = 2))
 
