4f52920bee9581a04265030fde84c9e08d5f1412
solveigvandervegt <sav57@cornell.edu>
2021-06-29 17:02:41 +0100

    Added R0(C)
    Added a plot of R0 as a function of C for different countries

--- R/R0.R
+++ R/R0.R
@@ -35,7 +35,7 @@ b = 0.15 # probability of infection
 k = 0.33 # 1/(incubation period in days)
 g = 0.1 # 1/(days between infection and recovery)
 m = 0.03 # probability of death, cases-fatality ratio.
-C = 0 # connectedness parameter
+C_def = 0 # connectedness parameter
 
 countries <- c("BDI","ETH","AFG","GIN","MLI","ROU","TUN","SAU","BRN","NLD")
 
@@ -44,7 +44,13 @@ store_phiY <- c()
 store_R0_basic <- c()
 store_comp <- c()
 
+C_range <- seq(0,1,0.001)
+n_C <- length(C_range)
+R0_of_C <- c()
+countries_store <- c()
+
 for (n in 1:length(countries)){
+  C <- C_def
   country = countries[n]
   print(country)
   # get fraction of population that is rural
@@ -100,10 +106,10 @@ for (n in 1:length(countries)){
   print(NU)
   NY <- sum(rowSums(mat_rural) * (pop_rural/sum(pop_rural))) / 2
   print(NY)
-  K <- ((1-frac_rural)* NU + frac_rural*NY)*C
+  K <- ((1-frac_rural)* NU + frac_rural*NY)
   
-  matF <- matrix(data = c(0, 0, b*S0U*(K + NU/(1-frac_rural)*(1-C)),              b*S0U*K,
-                          0, 0,              b*S0Y*K,                b*S0Y*(K + NY/frac_rural*(1-C)),     
+  matF <- matrix(data = c(0, 0, b*S0U*(K*C + NU/(1-frac_rural)*(1-C)),              b*S0U*K*C,
+                          0, 0,              b*S0Y*K*C,                b*S0Y*(K*C + NY/frac_rural*(1-C)),     
                           0, 0,                 0,                                   0,
                           0, 0,                 0,                                   0), 
               nrow = 4, ncol = 4, byrow = TRUE)
@@ -120,16 +126,40 @@ for (n in 1:length(countries)){
   } else {
     store_comp[n] <- c("\u2191")
   } 
+  
+  n1 <- (n-1)*n_C + 1
+  n2 <- n*n_C
+  countries_store[n1:n2] <- rep(country,n_C)
+  for (i in 1:n_C){
+    C <- C_range[i]
+    SQR <- ((1-frac_rural)*(K*C + NU/(1-frac_rural)*(1-C)))^2 + (frac_rural*(K*C + NY/frac_rural*(1-C)))^2 - 2*((1-frac_rural)*(K*C + NU/(1-frac_rural)*(1-C)))*(frac_rural*(K*C + NY/frac_rural*(1-C))) + 4*(K*C)^2*(frac_rural)*(1-frac_rural)
+    Lplus <- 0.5*b*(1-frac_rural)/(g+m)*(K*C + NU/(1-frac_rural)*(1-C)) + 0.5*b*frac_rural/(g+m)*(K*C + NY/frac_rural*(1-C)) + b/(2*(g+m))*sqrt(SQR)
+    Lmin <- 0.5*b*(1-frac_rural)/(g+m)*(K*C + NU/(1-frac_rural)*(1-C)) + 0.5*b*frac_rural/(g+m)*(K*C + NY/frac_rural*(1-C)) - b/(2*(g+m))*sqrt(SQR)
+    n3 <- (n-1)*length(C_range) + i
+    R0_of_C[n3] <- max(abs(Lplus),abs(Lmin))
+  }
 }
-countries <- c("BDI \n 86.6%R","ETH \n 78.8%R","AFG \n 74.2%R","GIN \n 63.5%R","MLI \n 56.9%R","ROU \n 45.9%R","TUN \n 30.7%R","SAU \n 15.9%R","BRN \n 22.1%R","NLD \n 8.1%R")
-df <- data.frame (country  = rep(countries,2), model = c(rep("basic",length(countries)),rep("UvsR",length(countries))), R0 = c(store_R0_basic,store_R0), relative = c(rep("",length(countries)),store_comp))
+
+
+countries2 <- c("BDI \n 86.6%R","ETH \n 78.8%R","AFG \n 74.2%R","GIN \n 63.5%R","MLI \n 56.9%R","ROU \n 45.9%R","TUN \n 30.7%R","SAU \n 15.9%R","BRN \n 22.1%R","NLD \n 8.1%R")
+df <- data.frame (country  = rep(countries2,2), model = c(rep("basic",length(countries)),rep("UvsR",length(countries))), R0 = c(store_R0_basic,store_R0), relative = c(rep("",length(countries)),store_comp))
 
 ggplot(data=df, aes(x=country, y=R0, fill = model)) +
   geom_bar(stat="identity",position=position_dodge()) + 
-  scale_x_discrete(limits=countries) +
+  scale_x_discrete(limits=countries2) +
   geom_abline(slope=0, intercept=1,  col = "red") +
   geom_text(aes(label = R0), colour = "white", size = 2,
             vjust = 1.5, position = position_dodge(.9)) +
   geom_text(aes(label = relative), colour = "white", size = 2,
-            vjust = 3.5, position = position_dodge(.9)) 
+            vjust = 3.5, position = position_dodge(.9)) + 
+  ggtitle("R0 of SEIRD and urban vs rural model for different countries")
+
+
+df2 <- data.frame(C = rep(C_range,length(countries)), R0 = R0_of_C, country = countries_store)
+df2$country <- factor(df2$country, levels = countries)
+ggplot(data = df2, aes(x = C, y = R0, color = country)) +
+  geom_line() + 
+  ggtitle("R0 for different values of C, by country")
+
+
 
