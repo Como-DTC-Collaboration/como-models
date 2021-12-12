fa2b7a5dc37a5c1706925252c2b477905d348809
ben18785 <ben.c.lambert@gmail.com>
2021-12-12 16:34:04 +0000

    first draft of SEIRD_BU model

--- R/SEIRD_BD.R
+++ R/SEIRD_BD.R
@@ -2,9 +2,9 @@
 #'
 NULL
 
-#' An S4 object representing the SEIRD.
+#' An S4 object representing the SEIRD_BD model.
 #'
-#' This class represents the SEIRD model, showing how populations of susceptible,
+#' This class represents the SEIRD_BD model, showing how populations of susceptible,
 #' exposed, infectious and recovered individuals evolve over time.
 #'
 #' @slot output_names list of compartments name which are used by the model and
@@ -12,7 +12,7 @@ NULL
 #' @slot initial_condition_names list of names of initial conditions
 #'       (characters). Default is list("S0", "E0", "I0", R0").
 #' @slot transmission_parameter_names list of names of transmission parameters
-#'       (characters). Default is list("beta", "kappa", "gamma", "mu").
+#'       (characters). Default is list("beta", "kappa", "gamma", "mu", "lambda", "nu").
 #' @slot initial_conditions list of values for initial conditions (double).
 #' @slot transmission_parameters list of values for transmission parameters
 #'       (double).
@@ -22,7 +22,7 @@ NULL
 #' @import reshape2
 #'
 #' @export SEIRD
-SEIRD <- setClass("SEIRD",
+SEIRD_BD <- setClass("SEIRD_BD",
          # slots
          slots = c(
            output_names = "list",
@@ -36,46 +36,46 @@ SEIRD <- setClass("SEIRD",
          prototype = list(
            output_names = list("S", "E", "I", "R", "D", "Incidence", "Deaths"),
            initial_condition_names = list("S0", "E0", "I0", "R0"),
-           transmission_parameter_names = list("beta", "kappa", "gamma", "mu"),
+           transmission_parameter_names = list("beta", "kappa", "gamma", "mu", "lambda", "nu"),
            initial_conditions = vector(mode = "list", length = 4),
            transmission_parameters = vector(mode = "list", length = 4)
          )
 )
 
 
-#' @describeIn SEIRD Retrieves initial conditions of SEIRD model.
+#' @describeIn SEIRD_BU Retrieves initial conditions of SEIRD_BU model.
 #'
-#' @param object An object of the class SEIRD.
+#' @param object An object of the class SEIRD_BU.
 #' 
 #' @export
-setMethod("initial_conditions", "SEIRD",
+setMethod("initial_conditions", "SEIRD_BU",
           function(object) object@initial_conditions)
 
 
-#' @describeIn SEIRD Retrieves transmission parameters of SEIR model.
+#' @describeIn SEIRD_BU Retrieves transmission parameters of SEIR model.
 #'
-#' @param object An object of the class SEIRD.
+#' @param object An object of the class SEIRD_BU.
 #' 
 #' @export
-setMethod("transmission_parameters", "SEIRD",
+setMethod("transmission_parameters", "SEIRD_BU",
           function(object) object@transmission_parameters)
 
-# SEIRD class specific functions
+# SEIRD_BU class specific functions
 
-#' @describeIn SEIRD Setter method for initial conditions (S0, E0, I0 and R0)
+#' @describeIn SEIRD_BU Setter method for initial conditions (S0, E0, I0 and R0)
 #' of the SEIR model.
 #'
 #' All initial conditions must sum up to 1.
 #' If the initial conditions provided to do not sum to 1, an error is thrown.
 #'
-#' @param object an object of the class SEIRD
+#' @param object an object of the class SEIRD_BU
 #' @param value (list) list of initial conditions S0, E0, I0, R0.
 #'
-#' @return object of class SEIRD with initial conditions assigned.
+#' @return object of class SEIRD_BU with initial conditions assigned.
 #' 
 #' @export
 setMethod(
-  "initial_conditions<-", "SEIRD",
+  "initial_conditions<-", "SEIRD_BU",
   function(object, value) {
 
     if (mean(names(value) %in% object@initial_condition_names) != 1)
@@ -102,21 +102,21 @@ setMethod(
   })
 
 
-#' @describeIn SEIRD Set transmission parameters (beta, kappa, gamma and mu)
-#' of the SEIRD model.
+#' @describeIn SEIRD_BU Set transmission parameters (beta, kappa, gamma, mu, lambda, nu)
+#' of the SEIRD_BU model.
 #'
 #' If the transmission parameters provided to are not 1-dimensional an error is
 #' thrown.
 #'
-#' @param object (SEIRD model)
-#' @param value (list) list of values for beta, kappa, gamma, mu, respectively.
+#' @param object (SEIRD_BU model)
+#' @param value (list) list of values for beta, kappa, gamma, mu, lambda, nu
 #'
-#' @return object of class SEIRD with transmission parameter values
+#' @return object of class SEIRD_BU with transmission parameter values
 #' assigned.
 #' 
 #' @export
 setMethod(
-  "transmission_parameters<-", "SEIRD",
+  "transmission_parameters<-", "SEIRD_BU",
   function(object, value) {
 
     # create list of parameter values
@@ -126,14 +126,16 @@ setMethod(
     trans_params <- value
 
     # check format of parameters
-    if (length(trans_params$b) != 1
-        | length(trans_params$k) != 1
-        | length(trans_params$g) != 1
-        | length(trans_params$m) != 1) {
+    if (length(trans_params$beta) != 1
+        | length(trans_params$kappa) != 1
+        | length(trans_params$gamma) != 1
+        | length(trans_params$mu) != 1
+        | length(trans_params$lambda) != 1
+        | length(trans_params$nu) != 1) {
       stop("The parameter values should be 1-dimensional.")
     }
 
-    # if all above tests are passed, assign the trans_params namelist to the
+    # if all above tests are passed, assign the trans_params named list to the
     # object
     object@transmission_parameters <- trans_params
 
@@ -141,20 +143,20 @@ setMethod(
   })
 
 
-#' @describeIn SEIRD Solves ODEs of the SEIRD specified in object
+#' @describeIn SEIRD_BU Solves ODEs of the SEIRD_BU specified in object
 #' for the time points specified in times and integration method specified in
 #' solve_method.
 #'
-#' \deqn{\frac{dS(t)}{dt} = - beta S(t) I(t)}
-#' \deqn{\frac{dE(t)}{dt} =  beta S(t) I(t) - kappa E(t)}
-#' \deqn{\frac{dI(t)}{dt} = kappa E(t) - (gamma + mu) I(t)}
-#' \deqn{\frac{dR(t)}{dt} = gamma I(t)}
+#' \deqn{\frac{dS(t)}{dt} = lambda - beta S(t) I(t) - nu S(t)}
+#' \deqn{\frac{dE(t)}{dt} =  beta S(t) I(t) - kappa E(t) - nu E(t)}
+#' \deqn{\frac{dI(t)}{dt} = kappa E(t) - (gamma + mu) I(t) - nu I(t)}
+#' \deqn{\frac{dR(t)}{dt} = gamma I(t) - nu R(t)}
+#' \deqn{\frac{dD(t)}{dt} = mu I(t) + nu (S(t) + E(t) + I(t) + R(t))}
 #' \deqn{\frac{dC(t)}{dt} = beta S(t) I(t)}
-#' \deqn{\frac{dD(t)}{dt} = mu I(t)}
 #'
 #' This function relies on the package deSolve.
 #'
-#' @param object an object of the class SEIRD
+#' @param object an object of the class SEIRD_BU
 #' @param times (double) a sequence of time points at which the solution to
 #' the system of ODEs should be returned. Must be of the form
 #' seq(t_start, t_end, by=t_step). Default time series is seq(0, 100, by = 1).
@@ -163,12 +165,12 @@ setMethod(
 #' the ode function in the deSolve package used in this function.
 #'
 #' @return two dataframes: one with the time steps, age range, time series of S,
-#' E, I and R population fractions, and one with the time steps, age range,
+#' E, I, R and D population fractions, and one with the time steps, age range,
 #' time series of incidences and deaths population fraction.
 #' 
 #' @export
 setMethod(
-  "run", "SEIRD",
+  "run", "SEIRD_BU",
   function(object, times, solve_method = "lsoda") {
     if (!is.double(times)) {
       stop("Evaluation times of the model storage format must be a vector.")
@@ -187,10 +189,12 @@ setMethod(
                C = 0,
                D = 0)
     # set transmission parameters vector
-    parameters <- c(b = transmission_parameters(object)$beta,
-                    k = transmission_parameters(object)$kappa,
-                    g = transmission_parameters(object)$gamma,
-                    m = transmission_parameters(object)$mu)
+    parameters <- c(beta = transmission_parameters(object)$beta,
+                    kappa = transmission_parameters(object)$kappa,
+                    gamma = transmission_parameters(object)$gamma,
+                    mu = transmission_parameters(object)$mu,
+                    lambda = transmission_parameters(object)$lambda,
+                    nu = transmission_parameters(object)$lambda)
     # function for RHS of ode system
     right_hand_side <- function(t, state, parameters) {
       with(
@@ -202,12 +206,12 @@ setMethod(
           c <- state[5]
           d <- state[6]
           # rate of change
-          ds <- -b * s * i
-          de <- b * s * i - k * e
-          di <- k * e - (g + m) * i
-          dr <- g * i
-          dc <- b * s * i
-          d_death <- m * i
+          ds <- lambda - beta * s * i - nu * s
+          de <- beta * s * i - kappa * e - nu * e
+          di <- kappa * e - (gamma + mu) * i - nu * i
+          dr <- gamma * i - nu * r
+          dc <- beta * s * i
+          d_death <- mu * i + nu * (s + e + i + r)
           # return the rate of change
           list(c(ds, de, di, dr, dc, d_death))
         })
@@ -235,7 +239,7 @@ setMethod(
     # Added for consistency of output format across models
     output$age_range <- rep("0-150", length(output$time))
 
-    # Split output into 2 dataframes: one with S,E,I, and R and one with C and D
+    # Split output into 2 dataframes: one with SEIRD, another with incidence and deaths
     states <- subset(output, !output$compartment %in% c("Incidence", "Deaths"))
     states <- droplevels(states)
     changes <- subset(output, output$compartment %in% c("Incidence", "Deaths"))
@@ -244,19 +248,21 @@ setMethod(
     list("states" = states, "changes" = changes)
   })
 
-#' @describeIn SEIRD Calculates basic reproduction number for SEIRD model
+#' @describeIn SEIRD_BU Calculates basic reproduction number for SEIRD_BU model
 #'
 #' The R0 parameter is given by:
-#' \deqn{R_0 = \beta/(\gamma + \mu)}
+#' \deqn{R_0 = \beta \kappa / (\kappa + \nu) * 1/(\gamma + \mu + \nu)}
 #'
-#' @param model an SEIRD model
+#' @param model an SEIRD_BU model
 #'
 #' @return an R0 value
 #' 
 #' @export
-setMethod("R0", "SEIRD", function(model) {
+setMethod("R0", "SEIRD_BU", function(model) {
   beta <- model@transmission_parameters$beta
   gamma <- model@transmission_parameters$gamma
   mu <- model@transmission_parameters$mu
-  beta / (gamma + mu)
+  kappa <- model@transmission_parameters$nu
+  nu <- model@transmission_parameters$nu
+  beta * kappa / (kappa + nu) * 1 / (gamma + mu + nu)
 })
