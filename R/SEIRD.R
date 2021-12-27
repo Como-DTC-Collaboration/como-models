48ae2af63d6a8bd58824af8ac6294ac25d5d52bd
ben18785 <ben.c.lambert@gmail.com>
2021-12-27 18:38:15 +0000

    corrected typo in docstrings of seird model

--- R/SEIRD.R
+++ R/SEIRD.R
@@ -4,7 +4,7 @@ NULL
 
 #' An S4 object representing the SEIRD.
 #'
-#' This class represents the SEIR model, showing how populations of susceptible,
+#' This class represents the SEIRD model, showing how populations of susceptible,
 #' exposed, infectious and recovered individuals evolve over time.
 #'
 #' @slot output_names list of compartments name which are used by the model and
@@ -52,7 +52,7 @@ setMethod("initial_conditions", "SEIRD",
           function(object) object@initial_conditions)
 
 
-#' @describeIn SEIRD Retrieves transmission parameters of SEIR model.
+#' @describeIn SEIRD Retrieves transmission parameters of SEIRD model.
 #'
 #' @param object An object of the class SEIRD.
 #' 
@@ -63,7 +63,7 @@ setMethod("transmission_parameters", "SEIRD",
 # SEIRD class specific functions
 
 #' @describeIn SEIRD Setter method for initial conditions (S0, E0, I0 and R0)
-#' of the SEIR model.
+#' of the SEIRD model.
 #'
 #' All initial conditions must sum up to 1.
 #' If the initial conditions provided to do not sum to 1, an error is thrown.
@@ -103,7 +103,7 @@ setMethod(
 
 
 #' @describeIn SEIRD Set transmission parameters (beta, kappa, gamma and mu)
-#' of the SEIR model.
+#' of the SEIRD model.
 #'
 #' If the transmission parameters provided to are not 1-dimensional an error is
 #' thrown.
